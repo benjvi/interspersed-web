85ad6257fdd965887d2ebc6456e3a36d8d52361b
Benjamin Vickers <ben881@gmail.com>
2018-08-07 17:03:46 -0600

    set admin user details explicitly

--- src/main/kotlin/com/benjvi/interspersedweb/WebSecurityConfig.kt
+++ src/main/kotlin/com/benjvi/interspersedweb/WebSecurityConfig.kt
@@ -1,5 +1,6 @@
 package com.benjvi.interspersedweb;
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -10,10 +11,32 @@ import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers.matchers
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.User.withDefaultPasswordEncoder
+import org.springframework.security.core.userdetails.UserDetails
+
+
 
 
 @EnableWebFluxSecurity
-class WebSecurityConfig {
+@Configuration
+class WebSecurityConfig(
+        @Value("\${admin_username}")
+        val usernameOne: String,
+        @Value("\${admin_password}")
+        val passwordOne: String
+    ){
+
+    @Bean
+    fun userDetailsRepository(): MapReactiveUserDetailsService {
+        val user = User.withDefaultPasswordEncoder()
+                .username(usernameOne)
+                .password(passwordOne)
+                .roles("USER", "ADMIN")
+                .build()
+        return MapReactiveUserDetailsService(user)
+    }
 
     @Bean
     fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
